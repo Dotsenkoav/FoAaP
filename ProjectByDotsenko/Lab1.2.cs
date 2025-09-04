ed2162aba76b5276cc4afb0104195698a73bd532
Dots <65274190+Dotsenkoav@users.noreply.github.com>
2025-09-04 17:34:21 +0700

    Add Lab1.2

---
+++ ProjectByDotsenko/Lab1.2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics; // Пространство имен числовых типов
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ProjectByDotsenko
+{
+    internal class LabTwoTaskTwo
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите стоимость килограмма яблок: ");
+            float costKgApple = float.Parse(Console.ReadLine().Replace(".", ","));
+            Console.Write("Введите кол-во килограмм яблок: ");
+            float weightApple = float.Parse(Console.ReadLine().Replace(".", ","));
+            float costApples = costKgApple * weightApple;
+            Console.WriteLine($"Стоимость: {costApples}");
+            Console.Read();
+        }
+    }
+}
