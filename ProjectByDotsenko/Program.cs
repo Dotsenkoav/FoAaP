d8ecfeb2fae77567d85bdf56b4159ba7b54dff4f
Dots <65274190+Dotsenkoav@users.noreply.github.com>
2025-09-04 17:12:41 +0700

    Добавьте файлы проекта.

---
+++ ProjectByDotsenko/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Numerics; // Пространство имен числовых типов
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ProjectByDotsenko
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputMassive = new List<int>(); // Создание пустого массива
+            Console.WriteLine("Введите последовательно положительные числа");
+            while (true) //Вход в цикл, выход из которого через ввод 0
+            {
+                string inputString = Console.ReadLine();
+                try
+                {
+                    int inputNum = int.Parse(inputString);
+                    if (inputNum == 0)
+                    {
+                        break;
+                    }
+                    else if (inputNum < 0)
+                    {
+                        Console.WriteLine("Введенно отрицательное число, возможен ввод ТОЛЬКО положительного");
+                    }
+                    else {
+                        inputMassive.Add(inputNum); //Добавление в массив значения
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ошибка ввода числа. Введите любое число или 0 для завершения программы");
+                    inputMassive.Clear();
+                }
+            }
+            if (inputMassive.Count == 0)
+            {
+                Console.WriteLine("Отсутствует ввод значения");
+            }
+            else
+            {
+                Console.WriteLine("Максимальное число в последовательности: " + inputMassive.Max());
+            }
+        }
+    }
+}
---
+++ ProjectByDotsenko/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ProjectByDotsenko")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjectByDotsenko")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("50aaa2f6-7328-4b30-a338-46dd850cd889")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
